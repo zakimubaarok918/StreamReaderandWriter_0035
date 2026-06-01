0f85c404c18be50744c25788f81e97e51367e1ac
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 10:31:47 +0700

    Implement main function to write to a file and prompt user

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -2,3 +2,16 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main()
+{
+
+    //deklarasi variabel baris untuk menyimpan data string
+    string baris;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar <=" << endl;
