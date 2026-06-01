5d9226283bea66c934614f3959560ee1725d8bfc
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 11:08:09 +0700

    Add newline for code formatting in ioFileWithPath.cpp

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+
     //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
 
@@ -18,3 +19,17 @@ int main()
     outfile.open(NamaFile + ".txt", ios::out);
 
     cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+    //looping untuk menulis isi file
+    while (true) 
+    {
+   
+        cout << "- ";
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi jika baris hanya berisi huruf q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+
+    }
