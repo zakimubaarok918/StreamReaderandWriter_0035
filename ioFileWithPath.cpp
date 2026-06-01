8849796f4660758f9e7e085168fa041ad565d8cd
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 11:10:23 +0700

    Add file closing and reading functionality in ioFileWithPath.cpp

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -33,3 +33,15 @@ int main()
         outfile << baris << endl;
 
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah nama file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">=Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
