7874f38762fbdf13a89c1965be9a9a41b40c3e02
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 11:05:32 +0700

    Add main function to handle file writing input and output

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,3 +3,18 @@
 #include <string>
 using namespace std;
 
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file: ";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi .txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
