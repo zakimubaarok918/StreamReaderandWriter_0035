6698a7ef4d2c358ed45df0822ed47f30151cfb8a
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 11:23:01 +0700

    Add try block for exception handling in main function of ExceptionStandard.cpp

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -8,3 +8,9 @@ int main()
 {
     cout << "Awal program" << endl; //penanda 1
     try
+    {
+        //deklarasi array data dengan panjang index 3
+        array<int, 3> data = {1, 2, 3};
+        //memanggil array element ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
+    }
