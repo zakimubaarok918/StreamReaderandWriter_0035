9a268e5752e62e736b49cb16ba2d527d97bc8a58
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 10:51:50 +0700

    Add main function with error handling demonstration

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dtampilkan" << endl;
+    }
