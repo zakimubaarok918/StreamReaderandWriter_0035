cc3aaf0937b05b55fb6d19bca0423df408409481
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 10:54:49 +0700

    Add exception handling for integer and default cases in ManualException

--- ManualException.cpp
+++ ManualException.cpp
@@ -10,3 +10,15 @@ int main()
         //throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dtampilkan" << endl;
     }
+    //kondisi jika throw yang bertipe data integer
+    catch (int e)
+    {
+        cout << "Pengecualian akan dieksekusi"<< endl;
+    }
+    //kondisi jika throw yang bertipe data selain integer
+    catch (...)
+    {
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
