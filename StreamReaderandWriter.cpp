57956cb4c1412b48c8b679b89ee5f1d12b61ee98
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-01 10:45:11 +0700

    Fix file reading logic and ensure proper closure of input file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -40,4 +40,16 @@ int main()
     cout << endl << ">=Membuka dan membaca file<=" << endl;
     //kondisi jika file ada
     if (infile.is_open())
-    
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai
+        infile.close();    
+    }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    return 0;
+
+}    
